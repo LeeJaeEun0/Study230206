5a41e91f5c7a2fbda6ca04482fa62c8e7e816e9f
LeeJaeEun0 <wodmschlrh12@naver.com>
2023-02-27 11:35:36 +0900

    update

--- Study230208/src/Ch04_Conditionals_Iterations/Ex23_Labeled_Loop.java
+++ Study230208/src/Ch04_Conditionals_Iterations/Ex23_Labeled_Loop.java
@@ -3,8 +3,8 @@ package Ch04_Conditionals_Iterations;
 public class Ex23_Labeled_Loop {
 
 	public static void main(String[] args) {
-		// for문에 Loop1이라는 
-		Loop1 : for (int i=2; i<=9; i++) { // 단
+		// foLoop1r문에 Loop1이라는 
+		 : for (int i=2; i<=9; i++) { // 단
 			for(int j=1; j<=9;j++) { // 곱하기
 				if(j==5)
 					//break Loop1;
