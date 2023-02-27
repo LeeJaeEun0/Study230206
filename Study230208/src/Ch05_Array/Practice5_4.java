928e122881de41a1a7846e226ef3a324f31c9d01
LeeJaeEun0 <wodmschlrh12@naver.com>
2023-02-27 12:07:32 +0900

    update

--- Study230208/src/Ch05_Array/Practice5_4.java
+++ Study230208/src/Ch05_Array/Practice5_4.java
@@ -4,23 +4,23 @@ public class Practice5_4 {
 
 	public static void main(String[] args) {
 		int[][] arr = {
-				{5, 5, 5, 5, 5},
-				{10, 10, 10, 10, 10},
-				{20, 20, 20, 20, 20},
-				{30, 30, 30, 30, 30}
+				{ 5, 5, 5, 5, 5 },
+				{ 10, 10, 10, 10, 10 },
+				{ 20, 20, 20, 20, 20 },
+				{ 30, 30, 30, 30, 30 }
 		};
-		
+
 		int total = 0;
 		float average = 0;
-		
+
 		// 알맞은 코드를 넣어 완성하시오 - 총합과 평균을 구하는 프로그래믈 구하시오
-		for(int i=0; i < arr.length; i++) {
-			for(int j=0; j < arr[i].length; j++)
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++)
 				total += arr[i][j];
 		} // end of for i
-		average = total/(arr.length * arr[0].length);
-		
-		System.out.println("total = "+ total);
+		average = total / (arr.length * arr[0].length); // 2차원 배열의 합계를 구하기 위함: arr.length * arr[0].length
+
+		System.out.println("total = " + total);
 		System.out.println("average =" + average);
 
 	} // end of main
--- Study230208/src/Ch05_Array/Practice5_6.java
+++ Study230208/src/Ch05_Array/Practice5_6.java
@@ -5,39 +5,38 @@ import java.util.Scanner;
 public class Practice5_6 {
 
 	public static void main(String[] args) {
-			String[] words = {"television", "computer", "mouse", "phone"};
-			
-			Scanner scanner = new Scanner(System.in);
-			
-			for(int i =0; i < words.length; i++) {
-				char[] question = words[i].toCharArray(); //string을 char로 변환
-				
-				// // 알맞은 코드를 작성하시오
-				// for(int j =0; i < question.length; j++) {
-				// 	int z = (int) (Math.random()*question.length);
-				// 	char tmp = question[j];
-				// 	question[j] = question[z];
-				// 	question[z] = tmp;
-				// }// end of for j
-				
-				for(int j=0;j<question.length;j++) {
-					int idx = (int)(Math.random() * question.length);
-					char tmp = question[i]; // i는 고정으로 두고 랜덤인 다른 숫자를 바꾸는 것
-					question[i] = question[idx];
-					question[idx] = tmp;
-					}
+		String[] words = { "television", "computer", "mouse", "phone" };
 
-				
-				System.out.printf("Q%d. %s의 정답을 입력하세요", i+1, new String(question));
-				
-				String answer = scanner.nextLine();
-				
-				//trim()으로 answer의 좌우 공백을 제거한 후 equals로 word[i]와 비교
-				if (words[i].equals(answer.trim()))
-					System.out.printf("맞았습니다.%n%n");
-				else
-					System.out.printf("틀렸습니다.%n%n");
-			} // end of for i
+		Scanner scanner = new Scanner(System.in);
+
+		for (int i = 0; i < words.length; i++) {
+			char[] question = words[i].toCharArray(); // string을 char로 변환
+
+			// // 알맞은 코드를 작성하시오
+			// for(int j =0; i < question[i].length; j++) {
+			// int z = (int) (Math.random()*question.length);
+			// char tmp = question[j];
+			// question[j] = question[z];
+			// question[z] = tmp;
+			// }// end of for j
+
+			for (int j = 0; j < question.length; j++) {
+				int idx = (int) (Math.random() * question.length);
+				char tmp = question[i]; // i는 고정으로 두고 랜덤인 다른 숫자를 바꾸는 것
+				question[i] = question[idx];
+				question[idx] = tmp;
+			}
+
+			System.out.printf("Q%d. %s의 정답을 입력하세요", i + 1, new String(question));
+
+			String answer = scanner.nextLine();
+
+			// trim()으로 answer의 좌우 공백을 제거한 후 equals로 word[i]와 비교
+			if (words[i].equals(answer.trim()))
+				System.out.printf("맞았습니다.%n%n");
+			else
+				System.out.printf("틀렸습니다.%n%n");
+		} // end of for i
 	} // end of main
 
 } // end of class
